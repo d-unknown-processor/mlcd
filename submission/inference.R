ef5b3af45cb5cf16c25042f2e10aee963b7b8df1
David Ryan Snyder <drsnyder@ugrad13.cs.jhu.edu>
2013-12-04 17:53:46 -0500

    Done with mean-field and structured mean-field

---
+++ submission/inference.R
@@ -0,0 +1,227 @@
+mean.field.inference <- function()
+{
+  q.a <- new.joint(1)
+  q.b <- new.joint(1)
+  q.c <- new.joint(1)
+  q.d <- new.joint(1)
+  q.e <- new.joint(1)
+  q.f <- new.joint(1)
+
+  a.up <- function(a)
+  {
+    numerator = 0
+    for (c in 1:2 ) {
+      for (b in 1:2 ) {
+        numerator = numerator + q.c[c]*q.b[b]*log(prob.a(a) * prob.b.given.a(b,a) * prob.c.given.ab(c,a,b))
+      }
+    }
+    exp(numerator)
+  }
+
+  b.up <- function(b)
+  {
+    numerator = 0
+
+    for (a in 1:2 ) {
+      for (c in 1:2) {
+        for (d in 1:2) {
+          numerator = numerator + q.d[d]*q.a[a]*q.c[c]*log(prob.d.given.b(d,b) * prob.c.given.ab(c,a,b) * prob.d.given.b(d,b))
+        }
+      }
+    }
+    
+    exp(numerator)
+  }
+
+  c.up <- function(c)
+  {
+    numerator = 0
+    for (a in 1:2 ) {
+      for (b in 1:2 ) {
+        for (e in 1:2 ) {
+          for (d in 1:2) {
+            numerator = numerator + q.d[d]*q.a[a]*q.e[e]*q.b[b]*log(prob.c.given.ab(c,a,b) * prob.e.given.cd(e,c,d))
+          }
+        }
+      }
+    }
+    
+    exp(numerator)
+  }
+
+  d.up <- function(d)
+  {
+    numerator = 0 
+    for (b in 1:2 ) {
+      for (c in 1:2) {
+        for (e in 1:2) {
+          for (f in 1:2) {
+              log_term = prob.d.given.b(d,b)*prob.f.given.d(f,d)*prob.e.given.cd(e,c,d)
+              numerator = numerator + q.c[c]*q.f[f]*q.e[e]*q.b[b]*log(log_term)
+          }
+        }
+      }
+    }
+    exp(numerator)
+  }
+
+  e.up <- function(e)
+  {
+    numerator = 0
+    for (c in 1:2 ) {
+      for (f in 1:2) {
+         for (d in 1:2) {
+           log_term = prob.e.given.cd(e,c,d)
+           numerator = numerator + q.c[c]*q.d[d]*q.f[f]*log(log_term)
+         }
+      }
+    }
+    exp(numerator)
+  }
+
+  f.up <- function(f)
+  {
+    numerator = 0
+    for (d in 1:2 ) {
+      for (e in 1:2 ) {
+        log_term = prob.f.given.d(f,d)
+        numerator = numerator + q.d[d]*q.e[e]*log(log_term)
+      }
+    }
+    exp(numerator)
+  }
+
+  niter <- 0
+  converged <- FALSE
+  tol <- 1e-3
+
+  close.enough <- function(q, q.old)
+  {
+    max(abs(q - q.old)) < tol
+  }
+
+  while (!converged)
+  {
+    q.a.old <- q.a
+    q.b.old <- q.b
+    q.c.old <- q.c
+    q.d.old <- q.d
+    q.e.old <- q.e
+    q.f.old <- q.f
+
+    q.a <- c(a.up(1), a.up(2)) / sum(a.up(1), a.up(2))
+    q.b <- c(b.up(1), b.up(2)) / sum(b.up(1), b.up(2))
+    q.c <- c(c.up(1), c.up(2)) / sum(c.up(1), c.up(2))
+    q.d <- c(d.up(1), d.up(2)) / sum(d.up(1), d.up(2))
+    q.e <- c(e.up(1), e.up(2)) / sum(e.up(1), e.up(2))
+    q.f <- c(f.up(1), f.up(2)) / sum(f.up(1), f.up(2))
+
+    niter <- niter + 1
+
+    converged <- all(close.enough(q.a, q.a.old),
+                     close.enough(q.b, q.b.old),
+                     close.enough(q.c, q.c.old),
+                     close.enough(q.d, q.d.old),
+                     close.enough(q.e, q.e.old),
+                     close.enough(q.f, q.f.old))
+  }
+
+  q.full <- function(a, b, c, d, e, f)
+  {
+    q.a[a] * q.b[b] * q.c[c] * q.d[d] * q.e[e] * q.f[f]
+  }
+
+  make.full.joint(q.full)
+}
+
+struct.mean.field.inference <- function()
+{
+  q.abc <- new.joint(3)
+  q.def <- new.joint(3)
+
+  q.b <-  function(b)    sum(q.abc[, b, ])
+  q.c <-  function(c)    sum(q.abc[, , c])
+  q.d <-  function(d)    sum(q.def[d, , ])
+  q.de <- function(d, e) sum(q.def[d, e, ])
+
+  abc.up <- function(a, b, c)
+  {
+    numerator = 0
+    for (d in 1:2 ) {
+      for (e in 1:2 ) {
+        log_part = prob.a(a)*prob.b.given.a(b,a)*prob.c.given.ab(c,a,b)*prob.d.given.b(d,b)*prob.e.given.cd(e,c,d)
+        q_part = q.d(d)*q.de(d,e)
+        numerator = numerator + q_part * log(log_part)
+      }
+    }
+    exp(numerator) 
+  }
+
+  def.up <- function(d, e, f)
+  {
+    numerator = 0
+    for (b in 1:2 ) {
+      for (c in 1:2 ) {
+        log_part = prob.f.given.d(f,d)*prob.e.given.cd(e,c,d)*prob.d.given.b(d,b)
+        q_part = q.b(b)*q.c(c)
+        numerator = numerator + q_part * log(log_part)
+      }
+    }
+    exp(numerator)
+  }
+
+  niter <- 0
+  converged <- FALSE
+  tol <- 1e-3
+
+  close.enough <- function(q, q.old)
+  {
+    max(abs(q - q.old)) < tol
+  }
+
+  while (!converged)
+  {
+    q.abc.old <- q.abc
+    q.def.old <- q.def
+
+    combs <- as.matrix(expand.grid(1:2, 1:2, 1:2))
+
+    ## Update A,B,C
+
+    for (i in seq_len(nrow(combs)))
+    {
+      a <- combs[i, 1]
+      b <- combs[i, 2]
+      c <- combs[i, 3]
+
+      q.abc[a, b, c] <- abc.up(a, b, c)
+    }
+
+    q.abc <- q.abc / sum(q.abc)
+
+    ## Update D,E,F
+
+    for (i in seq_len(nrow(combs)))
+    {
+      d <- combs[i, 1]
+      e <- combs[i, 2]
+      f <- combs[i, 3]
+
+      q.def[d, e, f] <- def.up(d, e, f)
+    }
+
+    q.def <- q.def / sum(q.def)
+
+    niter <- niter + 1
+
+    converged <- all(close.enough(q.abc, q.abc.old),
+                     close.enough(q.def, q.def.old))
+  }
+
+  q.full <- function(a, b, c, d, e, f)
+  {
+    q.abc[a, b, c] * q.def[d, e, f]
+  }
+
+  make.full.joint(q.full)
+}
